93d3da14e93cd8a4d6d0eed3f2c97487f7742d31
mwoelfer-tuwien <martinwoelfer01@gmail.com>
2020-10-03 23:25:20 +0200

    updated

---
+++ ld47/Assets/Scripts/TypeWriterEffect.cs
@@ -0,0 +1,38 @@
+﻿﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TypeWriterEffect : MonoBehaviour {
+
+	public float delay = 0.1f;
+	public string fullText;
+	private string currentText = "";
+
+	// Use this for initialization
+	void Start () {
+		StartCoroutine(ShowText());
+	}
+	
+	IEnumerator ShowText(){
+        fullText = fullText + " ";
+        var textCarret = true;
+		for(int i = 0; i < fullText.Length; i++){
+			currentText = fullText.Substring(0,i);
+            var delayAddition = 0.0f;
+            if (i != 0)
+            {
+                if (currentText.Substring(currentText.Length-1).Equals('/'))
+                {
+                    currentText = currentText.Substring(0, i-1);
+                    delayAddition = 0.5f;
+                }
+            }
+            if (i % 6 == 0 && (i!=fullText.Length-1))
+                textCarret = !textCarret;
+            if (textCarret)
+                currentText += 	" \u2588";
+			this.GetComponent<TMPro.TextMeshProUGUI>().text = currentText;
+			yield return new WaitForSeconds(delay + delayAddition);
+		}
+	}
+}
